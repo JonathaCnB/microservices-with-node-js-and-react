9c1b8a8b62b1b7f5ca93b60d20144f0dc8114c78
Jonatha Carlos <jotacarlosftb@gmail.com>
2022-05-26 20:21:02 -0400

    feat: implementing requests to services

--- client/src/components/CommentCreate/index.js
+++ client/src/components/CommentCreate/index.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const CommentCreate = () => {
+const CommentCreate = ({ postId }) => {
+    const [content, setContent] = useState('');
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+            content
+        });
+
+        setContent('');
+    }
   return (
-    <div>CommentCreate</div>
+    <div>
+        <form onSubmit={handleSubmit}>
+            <div className='form-group'>
+                <label>Novo Comentário</label>
+                <input
+                    type="text"
+                    className='form-control'
+                    value={content || ''} 
+                    onChange={(e) => setContent(e.target.value)}
+                />
+                <button type='submit' className='btn btn-primary'>Comentar</button>
+            </div>
+        </form>
+    </div>
   )
 }
 
